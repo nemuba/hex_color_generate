2e2a813420f576b77e38c1f137f7748b096a5f53
Alef Ojeda de Oliveira <alef.oliveira@dimensa.com.br>
2023-08-15 17:18:28 -0300

    add test

--- spec/hex_color_generate_spec.rb
+++ spec/hex_color_generate_spec.rb
@@ -1,11 +1,41 @@
 # frozen_string_literal: true
 
-RSpec.describe HexColorGenerate do
+RSpec.describe "HexColorGenerate" do
   it "has a version number" do
     expect(HexColorGenerate::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "has a constant COLORS" do
+    expect(HexColorGenerate::COLORS).not_to be nil
+  end
+
+  it "has a method generate" do
+    expect(HexColorGenerate).to respond_to(:generate)
+  end
+
+  it "has a method colors" do
+    expect(HexColorGenerate).to respond_to(:colors)
+  end
+
+  HexColorGenerate::COLORS.each do |color, _|
+    it "has a method #{color}" do
+      expect(HexColorGenerate).to respond_to(color)
+    end
+
+    it "has a method #{color}_keys" do
+      expect(HexColorGenerate).to respond_to("#{color}_keys")
+    end
+
+    it "has a method #{color}_values" do
+      expect(HexColorGenerate).to respond_to("#{color}_values")
+    end
+
+    it "return a color in hex format" do
+      expect(HexColorGenerate.send(color)).to match(/^#[0-9a-f]{6}$/)
+    end
+
+    it "return a color in hex format in string" do
+      expect(HexColorGenerate.send(color, type: color.to_s)).to be_a(String)
+    end
   end
 end
